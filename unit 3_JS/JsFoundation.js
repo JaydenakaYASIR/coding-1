306bebda09025d375841c2afbc86669665458088
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-10-31 19:17:36 +0000

    java scrip practice work 10/28/24

---
+++ unit 3_JS/JsFoundation.js
@@ -0,0 +1,30 @@
+// data types =most fundamental building block
+// of JS programming langiage.
+
+// strings data types- are character bound together
+// with quotation mark
+"anything wrappws in quotes"
+"it can be numbbers, letter, and symbols"
+"we can create strings with the double quotation marks."
+"we can also write string with single quotation marks."
+
+// Inteager data type- numbers
+// doesn't mater if its a whole or decimal number
+2908973
+182792
+32.452
+32434.3
+
+// boolean data type- true or false value
+true 
+false
+
+//variables - containers for data 
+// we create variables by doing the following:
+// 1. we need to declare the type of variable we are using.
+// 2. we need to give  the variable a name.
+// 3. we need to give it an equal sign.
+// 4. we need to give it data/ a value.
+var weather_city = 'philly' // universal variable keyword.
+let weather_temp = 70 // temporary/ expected to change.
+const forcast_weekly = 'sunny' //  constant/ doesnt change.
---
+++ unit 3_JS/jsPractice.js
@@ -0,0 +1,16 @@
+// Dataypes are the basic building
+// block of JS programs
+
+//Strings
+// strings are a price of dat that is wrapped in
+ //quotation marks.
+ "double quotes tell the computer that this a string ."
+ "my name is Yasir"
+ console.log('my name is Yasir');
+ //Integers
+ // integers are pieces of data that represent numbers.
+ 98
+ 89
+ console.log('3*12')
+ // Booleans
+ 
