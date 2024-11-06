0f0ed68fd861c57eb534f1f0aaa62405b55d0b93
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-11-06 20:10:20 +0000

    asyncActivity done

---
+++ unit 3_JS/asyncActivity_nov1.js
@@ -0,0 +1,29 @@
+// 1.decribe the difference between var, let . and const.
+"var - standard varible. short for varible. this can be used in any context "
+"let - This is a varible where the data may change "
+"const - This is a varible where the data needs to remain constant. Cannot change"
+
+// 2. Use what you've learned about data types and
+//varible and create three varibles that when multiplied
+// together will result in the value 4000. Research the console.log()
+// function on w3schools to output your result in the terminal.
+ 
+// keywords - words we know 
+// variable
+// data types
+// console.log
+
+//context -  what is it asking us to do 
+// mutiply 3 vars that will result in 4000
+var a = 2
+var b = 2 
+var c = 1000
+console.log("this message will apper in the teminal")
+console.log(a* b* c)
+
+
+
+"let - email draft that your still working on - can stil change "
+" const - email that has been sent- cannot change" 
+
+
