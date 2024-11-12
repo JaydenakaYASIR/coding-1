ac3c4e13232fc85043b56a4245b744c9e924eec6
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-11-12 20:18:10 +0000

    AsyncActiviy reveiw

---
+++ asyncActivityNOV8.js
@@ -0,0 +1,47 @@
+//1. what does "string concatenation" mean
+'string conncatenation is the process of combineing 2 strings together'
+var firstWord ='good afternoon'
+var secondword ='coding class'
+console.log(firstword + secondword)
+
+//2. please list and describe three rules for creating variable name.
+'varable name should confrom to the formatting rules; camel case, pascal'
+
+'1. variables connot start with numbers or symbols. they must start with a letter'
+
+'2. variable names are case sensative.'
+
+'3. variable names connot be a JS keyword/ reserved word'
+
+//3. Define each of the following variable name formats the provid 
+
+//# - camel case - every word after the first word is capitalized
+var firstNameLastName
+//#- snake case - every word is seperated with an undersoce.
+var first_Name_last_Name
+//#- Pascal case - every 
+
+//4. Find the missing variable for each of the following assignment operaor code snippets
+Python Version a = '?'
+
+b = a *100
+
+// the output in the terminal should be 300.
+
+// JavaScript Version let a = '?'
+
+let b = a *b
+
+console.log()// the output in the terminal should be 700.
+
+//5. Use the logical operators and evaluate the following variables to get the requested result.
+
+// which logical operator would you use to reflect that the result between the code expressions below are false. Use the console.log() function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+
+var x= 12
+
+var y= 2
+
+var z= 12/4
+
+console.log(x <= 12  z < 10)
