dc63896bd1d4bcf8f1793fc8c5cf574e3f7bfcba
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-11-13 20:14:40 +0000

    optratorReview

---
+++ optratorReview.js
@@ -0,0 +1,34 @@
+// Arithmetic oprators - Writting code expions
+// that perform math operations
+
+// examples of apps that use arithmetic operators
+
+// depositing money imto a digital account
+var depositAmount = 50;
+var checkingAccount = 1000;
+var savingsAccount =2000;
+
+console.log(depositAmount + checkingAccount); 
+// funtion - set of instructions that performs a job 
+
+// peer to peer money transfers
+var amountBeingSent =''
+var userAccount
+
+//Assigment - giving variables values.
+//we use the single equal sign (=) to assign data to variables
+
+// sneakers is the vaiable. We as  ASSIGNING the value of 200.00
+// with the equal sign.
+var sneakers = 200.00
+sneakers *= .30
+
+console.log(sneakers)
+
+// comparision - analyzing values. checking to see if a certain 
+// condition is met
+// ==
+//===
+//!
+
+//logical
