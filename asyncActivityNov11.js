1fa248055d185496099df47eb248edc97ffe5b25
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-11-15 20:11:52 +0000

    async work unfinshed

---
+++ asyncActivityNov11.js
@@ -0,0 +1,12 @@
+// 1. the optrator logical
+
+// 2. assigment
+var sneakers = 10.00
+sneakers *= .30
+
+//3.
+
+//4. for = operator checks whether its two operands are equal
+// for && operator for a set of boolean operands will be true if and only if all the operands are true. Otherwise it will be false
+// || operator for a set of operands is true if and only if one or more of its operands is true.
+// == operator checks whether its two operands are equal, returning a Boolean result
