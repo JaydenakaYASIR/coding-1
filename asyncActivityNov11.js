48545242ebe26a44c8b5d02b0fbbb4710f09859e
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-11-18 18:59:20 +0000

    async reveiw

--- asyncActivityNov11.js
+++ asyncActivityNov11.js
@@ -1,12 +1,22 @@
 // 1. the optrator logical
 
-// 2. assigment
-var sneakers = 10.00
-sneakers *= .30
+// 2. the optrator compareison
+var ECGOderRequest=3
+var ECGShipmentRecived=20
 
-//3.
+//3. the optrator assigment
+var messageToSend= 'hello class'
+var messageToBeRec= messeageToSend
 
-//4. for = operator checks whether its two operands are equal
-// for && operator for a set of boolean operands will be true if and only if all the operands are true. Otherwise it will be false
-// || operator for a set of operands is true if and only if one or more of its operands is true.
-// == operator checks whether its two operands are equal, returning a Boolean result
+//4. 
+//=  
+'single equal sign: assignment operator'
+
+//&&
+'double vertical amersand: logical AND operator'
+
+//||
+'double vertical slash: logical OR operator'
+
+//==
+'double equal: comparison SAME/EQUAL operator'
