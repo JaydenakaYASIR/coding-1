0d7936a884b5e9b5bae191d7f62f40a5f4fccd44
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-11-19 20:13:27 +0000

    function work

---
+++ unit 3_JS/functionPractice/function.js
@@ -0,0 +1,27 @@
+//What is function?
+'A function is a set intructions that performs a specific task'
+'or job'
+// Function come in 2 variatitons
+// built-in functions
+// user-defined functions- these are instructions that are pre-written
+//into the coding languge,ALL we need to do is pass in data.
+
+//examples
+var className= 'coding class'
+console.log(className)
+
+//user-defined functions- These are custom instrctions written by 
+//engineers to make code that performs specfic tasks.
+
+// How do we write a user-defined function?
+
+function examplesFunction(){
+    console.log('The function script is conncetd')
+}
+
+// In order to have a function work, we need to call it.
+// the above code is the function definition- MEANING-it's only 
+// telling the computer the instructions- NOT to actually run it.
+
+// we access or call a function by writting its name.
+examplesFunction()
