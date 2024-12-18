10e9c499a90bf0b7f0fe5ac9a5d38f140c796de3
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-12-18 20:12:51 +0000

    async work done

---
+++ asyncActivityDec13.js
@@ -0,0 +1,19 @@
+// - superShopper = 10% discount on their item
+// - megaShopper  = 15% discount on their item
+// - ultraShopper = 20% discount on their item
+
+function membershipDiscount(){
+    console.log("fuction is working");
+
+    if(member == "superShopper"){
+        console.log("you are a super shopper. You get 10 percent off.");
+    }
+
+    else if (member == "megaShopper"){
+        console.log("you are a super shopper. You get 10 percent off.");
+    }
+
+    else if (member == "ultraShopper"){
+        console.log("you are a ultra shopper. You get 10 percent off.");
+    }
+}
