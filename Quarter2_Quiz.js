e7ab822b741b8b634e985921d9ea21c2764bc9e4
JaydenakaYASIR <jheaden875@boyslatin.net>
2024-12-20 20:22:31 +0000

    javascriptQuiz not compelted

---
+++ Quarter2_Quiz.js
@@ -0,0 +1,8 @@
+// In your own words, describe what a function is. Please write your response in a complete sentence as a string data type.
+// a function is really like the body you cant strat anything without having the function
+
+//In your own words, describe the difference between a function parameter and a function argument. Please write your response in a complete sentenc as a string data typee.
+//fucntion parameter are names listed in the function/function argument are real values passed and received by the function
+
+//In your own words, describe what an if/ else conditional statement is. Please write your response in a complete sentence as a string data type.
+//
---
+++ holidayExtraCredit.js
@@ -0,0 +1,10 @@
+function Hoildayplanner(date){
+    let guest = 'Mike' 
+    if (name != guest ){
+        console.log("this name is not on the list");
+    } else{
+        console.log ("welcome to the party")
+    }
+    }
+    pizzaPartyInvitation('Yasir')
+    
