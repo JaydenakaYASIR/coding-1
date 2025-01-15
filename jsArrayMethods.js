e721171fca9d959f5a9b2e1e4bf3ce00e65e1e0a
JaydenakaYASIR <jheaden875@boyslatin.net>
2025-01-15 20:16:25 +0000

    arraymethod notes

---
+++ jsArrayMethods.js
@@ -0,0 +1,32 @@
+ var name = "YASIR";
+ var grocery = ["apple","milk","water", ];
+var, let, const, name
+
+ var generalList = [name1, grocery, 132323, 23.34, true];
+
+ 
+
+
+
+ // push method
+
+ generalList.push('new data')
+ console.log(generalList)
+
+
+ // Pop method
+
+
+
+
+
+generalList.pop()
+console.log(generalList)
+
+
+
+
+//Length 
+
+
+console.log(generalList.Length)
